197737b217980943c3ac81923320c3f67e0080ba
imkaushikgoswami <goswamikaushik2003@gmail.com>
2024-01-27 14:45:45 +0530

    i am starting javascript coding practice and also unserstand the important topics

---
+++ 1_use_strict.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// In JavaScript, "use strict"; is a directive that enables a stricter mode of interpretation by the browser or JavaScript engine. When you include "use strict"; at the beginning of a script or a function, it instructs the browser to execute the code in a strict operating context, enforcing more stringent rules and generating errors for common mistakes that would otherwise be ignored or would produce unpredictable behavior.
+
+// 1 - Catches Common Coding Mistakes
+// 2 - Improves Performance
+// 3 - Discourages Dangerous Features
+// etc ...
